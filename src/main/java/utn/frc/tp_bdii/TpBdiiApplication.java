4e34c201131cce1ee4921063555a490c95e826ff
412366_Marucco <412366@tecnicatura.frc.utn.edu.ar>
2025-05-22 15:55:35 -0300

    Initial Commit.

---
+++ src/main/java/utn/frc/tp_bdii/TpBdiiApplication.java
@@ -0,0 +1,13 @@
+package utn.frc.tp_bdii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpBdiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpBdiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utn/frc/tp_bdii/TpBdiiApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.frc.tp_bdii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpBdiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
